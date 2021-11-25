3d3efc533a610112fd483400ea0a77de5580d3dc
sanggayasa <sanggayasa502@gmail.com>
2021-11-25 16:13:41 +0700

    membuat API dengan framework HAPI , membuat route

--- server.js
+++ server.js
@@ -1,44 +1,16 @@
-/* eslint-disable indent */
-const http = require('http');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
-const requestListener = (request, response) => {
-	response.setHeader('Content-Type', 'text/html');
-	response.statusCode = 200;
+const init = async () => {
+	const server = Hapi.server({
+		port: 5000,
+		host: 'localhost',
+	});
 
-	const { method } = request;
+	server.route(routes);
 
-	if (method === 'GET') {
-		response.end('<h1>Hello!</h1>');
-	}
-
-	if (method === 'POST') {
-		let body = [];
-
-		request.on('data', (chunk) => {
-			body.push(chunk);
-		});
-
-		request.on('end', () => {
-			body = Buffer.concat(body).toString();
-			const { name } = JSON.parse(body);
-			response.end(`<h1>Hai, ${name}!</h1>`);
-		});
-	}
-
-	if (method === 'PUT') {
-		response.end('<h1>Bonjour!</h1>');
-	}
-
-	if (method === 'DELETE') {
-		response.end('<h1>Salam!</h1>');
-	}
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-const server = http.createServer(requestListener);
-
-const port = 5000;
-const host = 'localhost';
-
-server.listen(port, host, () => {
-	console.log(`Server berjalan pada http://${host}:${port}`);
-});
+init();
