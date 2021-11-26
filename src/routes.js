1470fd0e65e80c8ca27a703ddba8085429178248
sanggayasa <sanggayasa502@gmail.com>
2021-11-26 11:33:04 +0700

    hapus data

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const {
 	getAllNotesHandler,
 	getNoteByIdHandler,
 	editNoteByIdHandler,
+	deleteNoteByIdHandler,
 } = require('./handler');
 const routes = [
 	{
@@ -24,6 +25,11 @@ const routes = [
 		method: 'PUT',
 		path: '/notes/{id}',
 		handler: editNoteByIdHandler,
+	},
+	{
+		method: 'DELETE',
+		path: '/notes/{id}',
+		handler: deleteNoteByIdHandler,
 	},
 ];
 
